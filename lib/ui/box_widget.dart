9c1e24b004c0c07670df4087747c1e88e9299e57
superiorsd10 <sachindapkara6@gmail.com>
2024-03-16 20:47:46 +0530

    feat(ui): add the box widget

---
+++ lib/ui/box_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:autozoom_camera/models/recognition.dart';
+
+/// Individual bounding box
+class BoxWidget extends StatelessWidget {
+  final Recognition result;
+  final Function(Recognition) onSingleTap;
+
+  const BoxWidget({super.key, required this.result, required this.onSingleTap});
+
+  @override
+  Widget build(BuildContext context) {
+    // Color for bounding box
+    Color color = Colors.primaries[
+        (result.label.length + result.label.codeUnitAt(0) + result.id) %
+            Colors.primaries.length];
+
+    return Positioned(
+      left: result.renderLocation.left,
+      top: result.renderLocation.top,
+      width: result.renderLocation.width,
+      height: result.renderLocation.height,
+      child: GestureDetector(
+        onTap: () => onSingleTap(result),
+        child: Container(
+          width: result.renderLocation.width,
+          height: result.renderLocation.height,
+          decoration: BoxDecoration(
+              border: Border.all(color: color, width: 3),
+              borderRadius: const BorderRadius.all(Radius.circular(2))),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: FittedBox(
+              child: Container(
+                color: color,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(result.label),
+                    Text(" ${result.score.toStringAsFixed(2)}"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
